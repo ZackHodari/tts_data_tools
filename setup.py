d9f7e9945df6efd4eea9b1c454e166b77c58b5b9
ZackHodari <zackhodari@gmail.com>
2019-01-08 15:44:38 +0000

    Updated setup.py description.

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(
     name='tts_data_tools',
     version='0.1',
-    description='Modular TensorFlow wrapper for training models on temporal data.',
+    description='Data processing tools for preparing speech and labels for training TTS voices.',
     url='https://github.com/ZackHodari/tts_data_tools',
     author='Zack Hodari',
     author_email='zack.hodari@ed.ac.uk',
