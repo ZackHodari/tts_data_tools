3a83985ae6bfb481a1290411076156e7b3d026b0
ZackHodari <zackhodari@gmail.com>
2019-03-19 16:01:52 +0000

    Moved scripts within tts_data_tools package directory and updated console_scripts as this can only be used with packages that as built as part of setup.py

--- setup.py
+++ setup.py
@@ -1,5 +1,7 @@
 from setuptools import setup
 
+script_def = 'tdt_{name} = tts_data_tools.scripts.{name}:main'
+
 setup(
     name='tts_data_tools',
     version='0.3',
@@ -8,6 +10,12 @@ setup(
     author='Zack Hodari',
     author_email='zack.hodari@ed.ac.uk',
     # license='MIT',
+    install_requires=[
+        'numpy',
+        'pyworld',
+        'pyreaper',
+        'tqdm'
+    ],
     packages=['tts_data_tools'],
     package_data={'tts_data_tools': [
         'question_sets/questions-unilex_dnn_600.hed',
@@ -21,18 +29,19 @@ setup(
         'tdt_file_io = tts_data_tools.file_io:main',
         'tdt_lab_features = tts_data_tools.lab_features:main',
         'tdt_wav_features = tts_data_tools.wav_features:main',
-        'tdt_extract_counters = scripts.extract_counters:main',
-        'tdt_extract_durations = scripts.extract_durations:main',
-        'tdt_extract_lf0_and_vuv_reaper = scripts.extract_lf0_and_vuv_reaper:main',
-        'tdt_extract_numerical_labels = scripts.extract_numerical_labels:main',
-        'tdt_extract_numerical_labels_and_durations = scripts.extract_numerical_labels_and_durations:main',
-        'tdt_extract_phones = scripts.extract_phones:main',
-        'tdt_extract_world = scripts.extract_world:main',
-        'tdt_mean_variance_normalisation = scripts.mean_variance_normalisation:main',
-        'tdt_min_max_normalisation = scripts.min_max_normalisation:main',
-        'tdt_process_acoustics = scripts.process_acoustics:main',
-        'tdt_process_dataset = scripts.process_dataset:main',
-        'tdt_process_labels = scripts.process_labels:main'
+        script_def.format(name='extract_counters'),
+        script_def.format(name='extract_durations'),
+        script_def.format(name='extract_lf0_and_vuv_reaper'),
+        script_def.format(name='extract_numerical_labels'),
+        script_def.format(name='extract_numerical_labels_and_durations'),
+        script_def.format(name='extract_phones'),
+        script_def.format(name='extract_world'),
+        script_def.format(name='mean_variance_normalisation'),
+        script_def.format(name='min_max_normalisation'),
+        script_def.format(name='process_acoustics'),
+        script_def.format(name='process_dataset'),
+        script_def.format(name='process_labels')
     ]}
 )
+
 
