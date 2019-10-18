d95ba1d385829e07f0c1580783f88bca52cedb39
Zack Hodari <zackhodari@gmail.com>
2019-10-18 09:18:42 +0100

    Added custom_voice argument for Festival Utterance generation.

--- tts_data_tools/lab_gen/txt_to_utt.py
+++ tts_data_tools/lab_gen/txt_to_utt.py
@@ -21,18 +21,25 @@ from tts_data_tools import utils
 def add_arguments(parser):
     parser.add_argument("--festival_dir", action="store", dest="festival_dir", type=str, required=True,
                         help="Directory containing festival installation.")
-    parser.add_argument("--txt_file", action="store", dest="txt_file", type=str, default=None,
+    parser.add_argument("--txt_file", action="store", dest="txt_file", type=str, required=True,
                         help="File containing all transcriptions.")
-    parser.add_argument("--txt_dir", action="store", dest="txt_dir", type=str, default=None,
+    parser.add_argument("--txt_dir", action="store", dest="txt_dir", type=str, required=True,
                         help="Directory containing text transcriptions.")
-    parser.add_argument("--id_list", action="store", dest="id_list", type=str, default=None,
+    parser.add_argument("--id_list", action="store", dest="id_list", type=str, required=True,
                         help="List of file basenames to process (must be provided if txt_dir is used).")
     parser.add_argument("--out_dir", action="store", dest="out_dir", type=str, required=True,
                         help="Directory to save the output to.")
+    parser.add_argument("--custom_voice", action="store", dest="custom_voice", type=str, default=False,
+                        help="Name of Festival voice to use when generating Utterance structures..")
 
 
-def create_utterances(festival_dir, file_ids, sentences, out_dir):
+def create_utterances(festival_dir, file_ids, sentences, out_dir, custom_voice=None):
     scm_commands = []
+
+    if custom_voice is not None:
+        # Run Festival with a particular voice.
+        scm_commands.append(f'({custom_voice})')
+
     scm_command_str = '(utt.save (utt.synth (Utterance Text "{sentence}" )) "{utt_file}")'
 
     for file_id, sentence in zip(file_ids, sentences):
@@ -54,7 +61,7 @@ def create_utterances(festival_dir, file_ids, sentences, out_dir):
     subprocess.run([festival_exe, '-b', scm_file], check=True)
 
 
-def process_file(festival_dir, txt_file, out_dir):
+def process_file(festival_dir, txt_file, out_dir, custom_voice=None):
     """Create Utterance structures for all sentences in `txt_file` and save them to `out_dir`.
 
     Args:
@@ -92,10 +99,10 @@ def process_file(festival_dir, txt_file, out_dir):
     file_io.save_lines(file_ids, os.path.join(out_dir, 'file_id_list.scp'))
 
     # Create and save the Utterance structures.
-    create_utterances(festival_dir, file_ids, sentences, out_dir)
+    create_utterances(festival_dir, file_ids, sentences, out_dir, custom_voice=custom_voice)
 
 
-def process_dir(festival_dir, txt_dir, id_list, out_dir):
+def process_dir(festival_dir, txt_dir, id_list, out_dir, custom_voice=None):
     """Create Utterance structures for all sentences in `id_list` and save them to `out_dir`.
 
     Args:
@@ -118,21 +125,21 @@ def process_dir(festival_dir, txt_dir, id_list, out_dir):
     utils.make_dirs(os.path.join(out_dir, 'utts'), file_ids)
 
     # Create and save the Utterance structures.
-    create_utterances(festival_dir, file_ids, sentences, out_dir)
+    create_utterances(festival_dir, file_ids, sentences, out_dir, custom_voice=custom_voice)
 
 
-def process(festival_dir, txt_file=None, txt_dir=None, id_list=None, out_dir=None):
-    if (txt_file is None) != (txt_dir is None):
+def process(festival_dir, txt_file=None, txt_dir=None, id_list=None, out_dir=None, custom_voice=None):
+    if (txt_file is None) == (txt_dir is None):
         raise ValueError('Exactly one of txt_file or txt_dir must me specified.')
 
     if txt_dir is not None and id_list is None:
         raise ValueError('If txt_dir is used, id_list must be specified.')
 
     if txt_file is not None:
-        process_file(festival_dir, txt_file, out_dir)
+        process_file(festival_dir, txt_file, out_dir, custom_voice=custom_voice)
 
     if txt_dir is not None:
-        process_dir(festival_dir, txt_dir, id_list, out_dir)
+        process_dir(festival_dir, txt_dir, id_list, out_dir, custom_voice=custom_voice)
 
 
 def main():
@@ -141,7 +148,7 @@ def main():
     add_arguments(parser)
     args = parser.parse_args()
 
-    process(args.festival_dir, args.txt_file, args.txt_dir, args.id_list, args.out_dir)
+    process(args.festival_dir, args.txt_file, args.txt_dir, args.id_list, args.out_dir, args.custom_voice)
 
 
 if __name__ == "__main__":
